ede512c58bd73f7578357e36ab41d3797c9952f9
sajben94 <sajben94@gmail.com>
2017-10-30 20:05:19 +0000

    first commit

---
+++ index/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from index.models import AccessRecord,Topic,Webpage
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+
+# Register your models here.
