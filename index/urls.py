3c33dcc2977bd68d7260159eeb2b7a74ccf656e0
Darko Sajben <sajben94@gmail.com>
2017-10-31 18:37:18 +0100

    add help page

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from index import views
 
 urlpatterns = [
-    url(r'^$',views.hello,name="hello")
+    url(r'^$',views.hello,name="hello"),
+    url(r'^help/',views.help,name="help")
 ]
--- index/views.py
+++ index/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    my_dict = {'insert_me':'Now I am comming from index/hello'}
-    return render(request, 'hello.html',context=my_dict)
+    my_dict = {'insert_me':'Now I am comming from index/hello hahaha'}
+    return render(request, 'index/hello.html',context=my_dict)
 # Create your views here.
+
+def help(request):
+    my_dict = {'help_me':'This is help page!'}
+    return render(request,'index/help.html',context=my_dict)
